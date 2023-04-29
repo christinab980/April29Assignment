2e29974c8f35a501dca730c9db7a5af98e631298
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-04-29 09:28:16 -0700

    adding file tree and server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const server = express();
+
+server.get('/heartbeat',(req, res) => {
+    res.json({
+        "is":"working",
+    });
+});
+
+server.listen(8080, () => {
+    console.log("This server is running at PORT 8080")
+});
