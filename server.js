be9869e8257a73ceffd25e3007b6c51784697973
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-04-29 09:45:01 -0700

    added checkAuth middleware

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+es6Renderer = require('express-es6-template-engine');
 
 const server = express();
+
+// server.use("/", express.static(__dirname + "/public"));
+
+server.engine('html', es6Renderer);
+server.set('views', 'views')
+server.set('view engine', 'html')
 
 server.get('/heartbeat',(req, res) => {
     res.json({
